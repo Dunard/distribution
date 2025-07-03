816a9b4aca49a6cc8d4be2529c194a0ae8929f4e
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-07-03 06:39:07 -0500

    Update package.mk

--- packages/emulators/libretro/ppsspp-lr/package.mk
+++ packages/emulators/libretro/ppsspp-lr/package.mk
@@ -20,7 +20,7 @@
 ################################################################################
 
 PKG_NAME="ppsspp-lr"
-PKG_VERSION="58e12acee4d7829ee675ce95f3d377c3aaa3ecc5"
+PKG_VERSION="1ce60824761b6bb288ad4e3301e23bd984e617fb"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/hrydgard/ppsspp"
 PKG_URL="https://github.com/hrydgard/ppsspp.git"
