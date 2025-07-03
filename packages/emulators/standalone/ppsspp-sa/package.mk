0165d56f7d076eabd46ffe6a489abc4acbbb2a12
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-07-03 06:39:34 -0500

    Update package.mk

--- packages/emulators/standalone/ppsspp-sa/package.mk
+++ packages/emulators/standalone/ppsspp-sa/package.mk
@@ -5,7 +5,7 @@
 PKG_NAME="ppsspp-sa"
 PKG_SITE="https://github.com/hrydgard/ppsspp"
 PKG_URL="${PKG_SITE}.git"
-PKG_VERSION="c017718b51e1d8aa4acf16aa9b096a43594c807a" # v1.19.2
+PKG_VERSION="1ce60824761b6bb288ad4e3301e23bd984e617fb" # v1.19.2
 CHEAT_DB_VERSION="31d7280ed5bad454df5bddc6d953de84f34c9ef5" # Update cheat.db (26/06/2024)
 PKG_LICENSE="GPLv2"
 PKG_DEPENDS_TARGET="toolchain ffmpeg libzip SDL2 zlib zip"
