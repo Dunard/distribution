b73000efe12c63c98b3b0608a0cb3875a4283138
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-07-03 05:39:28 -0500

    Update package.mk

--- packages/emulators/libretro/gearsystem-lr/package.mk
+++ packages/emulators/libretro/gearsystem-lr/package.mk
@@ -20,7 +20,7 @@
 ################################################################################
 
 PKG_NAME="gearsystem-lr"
-PKG_VERSION="81decc29b8aada7b206934bc970531a35ca1a080"
+PKG_VERSION="c202db46408320ae52fc2e639d6bfa2f09f8ee70"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/drhelius/Gearsystem"
 PKG_URL="${PKG_SITE}.git"
