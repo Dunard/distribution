8aeda4dfcdde43eb64ffe77d362e456aa8334e18
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-07-05 22:28:30 -0500

    Update package.mk

--- packages/emulators/libretro/beetle-psx-lr/package.mk
+++ packages/emulators/libretro/beetle-psx-lr/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="beetle-psx-lr"
-PKG_VERSION="5b448437dfa0cd246a655ffc76b04a1be08c3af9"
+PKG_VERSION="42dc44815131079db0de69d2b1d52e9f7edf842e"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://git.libretro.com/libretro/beetle-psx-libretro"
 PKG_URL="${PKG_SITE}.git"
