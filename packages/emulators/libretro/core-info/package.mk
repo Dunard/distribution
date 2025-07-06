89640e7220e8dc71c160c3cc36643e85d713212e
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-07-05 22:29:53 -0500

    Update package.mk

--- packages/emulators/libretro/core-info/package.mk
+++ packages/emulators/libretro/core-info/package.mk
@@ -20,7 +20,7 @@
 ################################################################################
 
 PKG_NAME="core-info"
-PKG_VERSION="98016cb2ee6447f98deaf9a7f8743516c78b4bbe"
+PKG_VERSION="afd13d4c7aa61d5111fe0fbdcf80f1d8571046fd"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-core-info"
 PKG_URL="https://github.com/libretro/libretro-core-info/archive/${PKG_VERSION}.tar.gz"
