b0d8e6cdcba743212e03aaae99058b482dbfde10
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-07-06 09:33:15 -0500

    Update package.mk

--- packages/emulators/libretro/genesis-plus-gx-lr/package.mk
+++ packages/emulators/libretro/genesis-plus-gx-lr/package.mk
@@ -21,7 +21,7 @@
 ################################################################################
 
 PKG_NAME="genesis-plus-gx-lr"
-PKG_VERSION="7eaca304a77827d4ba0f9bd9e23aca1ed8eb231e"
+PKG_VERSION="17c9c5e98cc379b708a081bbf40d939c2481b1e8"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/libretro/Genesis-Plus-GX"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
