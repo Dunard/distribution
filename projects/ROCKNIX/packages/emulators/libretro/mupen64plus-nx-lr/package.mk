89e555423513fafa123ec61267f8a73fee86d42f
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-08-15 08:19:32 -0500

    Update package.mk

--- projects/ROCKNIX/packages/emulators/libretro/mupen64plus-nx-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/mupen64plus-nx-lr/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="mupen64plus-nx-lr"
-PKG_VERSION="7c7f11061f29b2ccd5d0952e2373ae5b55cfea8f"
+PKG_VERSION="2d923939db32aad01e633010fac71f860c943a6d"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mupen64plus-libretro-nx"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
