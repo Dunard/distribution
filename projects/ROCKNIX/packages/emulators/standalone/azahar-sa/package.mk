53b7d842f01c1a4437c2a71dc59ad2acf0761f5f
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-08-17 00:23:15 -0500

    Update package.mk

--- projects/ROCKNIX/packages/emulators/standalone/azahar-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/azahar-sa/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="azahar-sa"
-PKG_VERSION="cfc96e993b570578daf55640123287236c044b55" # tag AZAHAR_PLUS_2122_A
+PKG_VERSION="8786f733836c56761fe897200feb20a8967fc9ed"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/AzaharPlus/AzaharPlus"
 PKG_URL="${PKG_SITE}.git"
