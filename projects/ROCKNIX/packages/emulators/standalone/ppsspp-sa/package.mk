694a478e41a86fd93878b580764b43ea8942b5cf
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-08-19 02:15:24 -0500

    Update package.mk

--- projects/ROCKNIX/packages/emulators/standalone/ppsspp-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/ppsspp-sa/package.mk
@@ -5,7 +5,7 @@
 PKG_NAME="ppsspp-sa"
 PKG_SITE="https://github.com/hrydgard/ppsspp"
 PKG_URL="${PKG_SITE}.git"
-PKG_VERSION="794efe368196a996f90b71f43ff3b23971e992c8" # v1.19.3
+PKG_VERSION="9a50fa4834bb1792168754135d95bcf1a3bd8de3" # v1.19.3
 CHEAT_DB_VERSION="31d7280ed5bad454df5bddc6d953de84f34c9ef5" # Update cheat.db (26/06/2024)
 PKG_LICENSE="GPLv2"
 PKG_DEPENDS_TARGET="toolchain ffmpeg libzip SDL2 zlib zip"
