6631ee4354058437b476c1f9b2952459ea309087
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-09-02 00:11:29 -0500

    Update package.mk

--- projects/ROCKNIX/packages/emulators/libretro/mame-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/mame-lr/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
 PKG_NAME="mame-lr"
-PKG_VERSION="d9f594146b6c43b7b15ee2569d1175e62030f0cb"
+PKG_VERSION="b943eb3a60f5c25b7c788f5db4efe043c3635158"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/mame"
 PKG_URL="https://github.com/libretro/mame/archive/${PKG_VERSION}.tar.gz"
