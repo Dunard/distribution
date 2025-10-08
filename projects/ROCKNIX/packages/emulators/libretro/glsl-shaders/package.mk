cbdd25dd21f99e180d00d163eb5e743ffebe91dd
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-10-07 23:35:46 -0500

    Update package.mk

--- projects/ROCKNIX/packages/emulators/libretro/glsl-shaders/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/glsl-shaders/package.mk
@@ -21,7 +21,7 @@
 ################################################################################
 
 PKG_NAME="glsl-shaders"
-PKG_VERSION="3a71b1d20edbf06d56e79072c86b5edb393737a1"
+PKG_VERSION="b935c6970b9b2bf6c81bf565d54012e947a838d8"
 PKG_ARCH="aarch64"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/glsl-shaders"
