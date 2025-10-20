245b06a3e50132378f489dda71c50c6952e2f348
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-10-19 22:21:42 -0500

    Updated libretro-database sha1

--- projects/ROCKNIX/packages/emulators/libretro/libretro-database/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/libretro-database/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="libretro-database"
-PKG_VERSION="d3075ab69659d351ae3aea7dc4ba53e21d3d9d1e"
+PKG_VERSION="ce4575de4665e23263e01d5f071752596c58945d"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-database"
 PKG_URL="https://github.com/libretro/libretro-database/archive/${PKG_VERSION}.tar.gz"
