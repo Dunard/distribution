d09f7f231eeea066b84eb42067e37809ae63e8a1
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-10-19 21:21:25 -0500

    Create Picotron start_picotron.sh

---
+++ projects/ROCKNIX/packages/emulators/libretro/picotron/sources/start_picotron.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
+
+# Source predefined functions and variables
+. /etc/profile
+
+GAME_DIR="/storage/roms/picotron/"
+
+case ${HW_ARCH} in
+  aarch64)
+    STATIC_BIN="picotron"
+  ;;
+esac
+
+# check if the file being launched contains "load-picotron" and if so launch Picotron
+shopt -s nocasematch
+if [[ "${1}" == *load-picotron* ]]; then
+  OPTIONS="-load-picotron"
+else
+  OPTIONS="-run"
+  CART="${1}"
+fi
+shopt -u nocasematch
+
+INTEGER_SCALE=$(get_setting picotron.integerscale)
+if [ "${INTEGER_SCALE}" = "1" ]
+then
+  OPTIONS="${OPTIONS} -pixel_perfect 1"
+fi
+
+if [ -d "${GAME_DIR}/${HW_ARCH}" ]
+then
+  LAUNCH_DIR="${GAME_DIR}/${HW_ARCH}"
+else
+  LAUNCH_DIR="${GAME_DIR}"
+fi
+
+# store sdl_controllers in root directory so its shared across devices - will look to revisit this with controller refactor work
+cp -f /usr/config/SDL-GameControllerDB/gamecontrollerdb.txt ${GAME_DIR}/sdl_controllers.txt
+
+# mark the binary executable to cover cases where the user adding the binaries doesn't know or forgets.
+chmod 0755 ${LAUNCH_DIR}/${STATIC_BIN}
+set_kill set "-9 ${STATIC_BIN} start_picotron.sh"
+${LAUNCH_DIR}/${STATIC_BIN} -home -root_path ${GAME_DIR} -joystick 0 ${OPTIONS} "${CART}"
