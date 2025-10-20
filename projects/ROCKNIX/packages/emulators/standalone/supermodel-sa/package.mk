0cd76238871179a02ac1dbb044ee654191629436
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-10-19 21:57:30 -0500

    Updated supermodel-sa arm sha1

--- projects/ROCKNIX/packages/emulators/standalone/supermodel-sa/package.mk
+++ projects/ROCKNIX/packages/emulators/standalone/supermodel-sa/package.mk
@@ -12,7 +12,7 @@ GET_HANDLER_SUPPORT="git"
 
 case ${TARGET_ARCH} in
   aarch64|arm)
-    PKG_VERSION="b13fdd5029a246e8f90a1d70e2ed000779fee4f1"
+    PKG_VERSION="108386afb84b2bbab968e8640444fd8201309152"
     PKG_GIT_CLONE_BRANCH="arm"
   ;;
   *)
