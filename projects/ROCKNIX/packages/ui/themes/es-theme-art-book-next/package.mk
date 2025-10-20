0e6bd90578f756a358feb650e222c88e9b1ae10a
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-10-19 22:39:07 -0500

    Updated es-theme-art-book-next sha1

--- projects/ROCKNIX/packages/ui/themes/es-theme-art-book-next/package.mk
+++ projects/ROCKNIX/packages/ui/themes/es-theme-art-book-next/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="es-theme-art-book-next"
-PKG_VERSION="5db8b08c41be87843cf47e8b9de605ed7d26c81b"
+PKG_VERSION="80ecb042e93d38cec6277446e483eba387708f38"
 PKG_LICENSE="CUSTOM"
 PKG_SITE="https://github.com/anthonycaccese/art-book-next-es"
 PKG_URL="https://github.com/anthonycaccese/art-book-next-es/archive/${PKG_VERSION}.tar.gz"
