1bfbf586f9ced3381a206d917687facf8ff722d9
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-10-31 10:28:34 -0500

    Update package version for gearsystem-lr

--- projects/ROCKNIX/packages/emulators/libretro/gearsystem-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/gearsystem-lr/package.mk
@@ -20,7 +20,7 @@
 ################################################################################
 
 PKG_NAME="gearsystem-lr"
-PKG_VERSION="81decc29b8aada7b206934bc970531a35ca1a080"
+PKG_VERSION="ca76b0b381b9af8bbde3d7fabafda07bd3bd624d"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/drhelius/Gearsystem"
 PKG_URL="${PKG_SITE}.git"
