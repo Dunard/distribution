dba1cff3f3031b312a75b045ca1af0c88292f209
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-10-31 09:02:41 -0500

    Create package.mk for dice-lr emulator
    Added package.mk for dice-lr emulator with metadata and build instructions.

---
+++ projects/ROCKNIX/packages/emulators/libretro/dice-lr/package.mk
@@ -0,0 +1,23 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2025-present ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="dice-lr"
+PKG_VERSION="ecdfaf2e855d135132fe2353918039901a21f790"
+PKG_LICENSE="GPLv3"
+PKG_SITE="https://github.com/mittonk/dice-libretro"
+PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
+# PKG_SITE="https://git.libretro.com/libretro/dice-libretro"
+# PKG_URL="${PKG_SITE}/-/archive/${PKG_VERSION}/dice-libretro-${PKG_VERSION}.tar.gz"
+PKG_DEPENDS_TARGET="toolchain"
+PKG_LONGDESC="DICE is a Discrete Integrated Circuit Emulator. It emulates computer systems that lack any type of CPU, consisting only of discrete logic components."
+
+PKG_TOOLCHAIN="make"
+
+make_target() {
+   make -C ${PKG_BUILD} -f Makefile
+}
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/lib/libretro
+  cp dice_libretro.so ${INSTALL}/usr/lib/libretro/
+}
