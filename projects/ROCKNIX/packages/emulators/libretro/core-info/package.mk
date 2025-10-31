9305f52e4d4f63d8531db543aff83dd9f4a40859
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-10-31 10:24:02 -0500

    Update package version for core-info

--- projects/ROCKNIX/packages/emulators/libretro/core-info/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/core-info/package.mk
@@ -20,7 +20,7 @@
 ################################################################################
 
 PKG_NAME="core-info"
-PKG_VERSION="98016cb2ee6447f98deaf9a7f8743516c78b4bbe"
+PKG_VERSION="bc4abc9daff498eaa479a7d1efacb5ea97d2a79a"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-core-info"
 PKG_URL="https://github.com/libretro/libretro-core-info/archive/${PKG_VERSION}.tar.gz"
