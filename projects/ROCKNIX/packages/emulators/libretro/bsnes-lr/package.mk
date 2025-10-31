bd444d6079709b4f1fef72e5a67e3e99258ac2dc
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-10-31 10:23:10 -0500

    Update bsnes-lr package version

--- projects/ROCKNIX/packages/emulators/libretro/bsnes-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/bsnes-lr/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="bsnes-lr"
-PKG_VERSION="b102d6d5817b25aa059b573cd3b7675f2e375fa4"
+PKG_VERSION="57155d8037463346307123daabeaa27298e0f956"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/bsnes-libretro"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
