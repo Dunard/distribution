e21798343032874b1cb82a684a9e86c0eb1b817b
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-11-22 20:07:45 -0600

    Update gearcoleco-lr package version

--- projects/ROCKNIX/packages/emulators/libretro/gearcoleco-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/gearcoleco-lr/package.mk
@@ -15,7 +15,7 @@
 ################################################################################
 
 PKG_NAME="gearcoleco-lr"
-PKG_VERSION="f3e3e1812a3421bf24fca26ff257fb36842d33a9"
+PKG_VERSION="8598f1bc43d49f0e1b4e257e3691096ee543dba5"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/drhelius/Gearcoleco"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
