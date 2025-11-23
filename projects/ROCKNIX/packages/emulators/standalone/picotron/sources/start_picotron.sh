3e37e4e23a172820bb7f129edcc78d1389e6c472
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-11-22 19:42:08 -0600

    Create start_picotron.sh to launch Picotron emulator (picotron)

---
+++ projects/ROCKNIX/packages/emulators/standalone/picotron/sources/start_picotron.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+# Source predefined functions and variables
+. /etc/profile
+
+GAME_DIR="/storage/roms/picotron/"
+
+case ${HW_ARCH} in
+  aarch64)
+    STATIC_BIN="picotron"
+  ;;
+esac
+
+# I don't know this will work or not. integerscale shown on the start_pico8.sh.
+INTEGER_SCALE=$(get_setting picotron.integerscale)
+if [ "${INTEGER_SCALE}" = "1" ]
+then
+  OPTIONS="${OPTIONS} -pixel_perfect 1"
+fi
+
+if [ -d "${GAME_DIR}/${HW_ARCH}" ]
+then
+  LAUNCH_DIR="${GAME_DIR}/${HW_ARCH}"
+else
+  LAUNCH_DIR="${GAME_DIR}"
+fi
+
+# store sdl_controllers in root directory so its shared across devices - will look to revisit this with controller refactor work
+cp -f /usr/config/SDL-GameControllerDB/gamecontrollerdb.txt ${GAME_DIR}/sdl_controllers.txt
+
+# mark the binary executable to cover cases where the user adding the binaries doesn't know or forgets.
+chmod 0755 ${LAUNCH_DIR}/${STATIC_BIN}
+set_kill set "-9 ${STATIC_BIN} start_picotron.sh"
+${LAUNCH_DIR}/${STATIC_BIN} -home -root_path ${GAME_DIR} -joystick 0
