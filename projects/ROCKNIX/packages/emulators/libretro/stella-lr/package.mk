53b1ddd06e74179e2f19b35810efdad431b914d1
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-11-27 18:14:22 -0600

    Update stella-lr package version

--- projects/ROCKNIX/packages/emulators/libretro/stella-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/stella-lr/package.mk
@@ -20,7 +20,7 @@
 ################################################################################
 
 PKG_NAME="stella-lr"
-PKG_VERSION="5d737b2e54717753be235c4fd2cd172e307da623"
+PKG_VERSION="ec7ad887c777a7924fdc786a9c5901e65d4c6cd0"
 PKG_LICENSE="GPL2"
 PKG_SITE="https://github.com/stella-emu/stella"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
