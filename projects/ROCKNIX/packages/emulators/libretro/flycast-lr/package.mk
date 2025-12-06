bc36842438e08b78968dba77c0286d309ec014d5
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-12-05 18:40:20 -0600

    Update flycast-lr package version

--- projects/ROCKNIX/packages/emulators/libretro/flycast-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/flycast-lr/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="flycast-lr"
-PKG_VERSION="5a150f41218f4350161e2b55178e1feb978b36c7"
+PKG_VERSION="230d971d4d09747c8c107f740d85a63f9f15b646"
 PKG_SITE="https://github.com/flyinghead/flycast"
 PKG_URL="${PKG_SITE}.git"
 PKG_DEPENDS_TARGET="toolchain zlib libzip"
