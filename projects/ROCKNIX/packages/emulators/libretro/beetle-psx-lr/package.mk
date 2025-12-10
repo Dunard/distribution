60904a754efb3e854bd48dfffe27d2ee9b6e0728
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-12-10 05:49:06 -0600

    Update beetle-psx-lr package version

--- projects/ROCKNIX/packages/emulators/libretro/beetle-psx-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/beetle-psx-lr/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2022-present JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="beetle-psx-lr"
-PKG_VERSION="4e0cb4ddf0c52ef802cd4f7f2b7d3a187ab9962d"
+PKG_VERSION="1420c90299b1d9f4ef59373b3e5fa538a1e17a65"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://git.libretro.com/libretro/beetle-psx-libretro"
 PKG_URL="${PKG_SITE}.git"
