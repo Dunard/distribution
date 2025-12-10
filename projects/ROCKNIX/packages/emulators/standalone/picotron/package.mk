75220786ab5d342dfd09330d12bff6769f2d3297
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-12-10 05:34:47 -0600

    Create package.mk for Picotron emulator (picotron)

---
+++ projects/ROCKNIX/packages/emulators/standalone/picotron/package.mk
@@ -0,0 +1,29 @@
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
+
+PKG_NAME="picotron"
+PKG_VERSION="17c0eeda3f83aba2183022f7f36d96174854ec33"
+PKG_LICENSE="GPLv2"
+PKG_SITE=""
+PKG_URL=""
+PKG_DEPENDS_TARGET="toolchain SDL2"
+PKG_LONGDESC="Picotron Fantasy Console"
+PKG_TOOLCHAIN="manual"
+
+if [ ! "${OPENGL}" = "no" ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGL} glu libglvnd"
+fi
+
+if [ "${OPENGLES_SUPPORT}" = yes ]; then
+  PKG_DEPENDS_TARGET+=" ${OPENGLES}"
+fi
+
+makeinstall_target() {
+  mkdir -p ${INSTALL}/usr/bin
+  cp ${PKG_DIR}/sources/start_picotron.sh ${INSTALL}/usr/bin
+  chmod 0755 ${INSTALL}/usr/bin/start_picotron.sh
+
+  mkdir -p ${INSTALL}/usr/lib/autostart/common
+  cp ${PKG_DIR}/sources/autostart/common/* ${INSTALL}/usr/lib/autostart/common
+  chmod 0755 ${INSTALL}/usr/lib/autostart/common/*
+}
