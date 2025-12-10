3b04159e76ac612352a63471fbb899915be1e6cf
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-12-10 06:15:14 -0600

    Update picodrive-lr package version

--- projects/ROCKNIX/packages/emulators/libretro/picodrive-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/picodrive-lr/package.mk
@@ -1,5 +1,5 @@
 PKG_NAME="picodrive-lr"
-PKG_VERSION="c4332d608c1005a46ce51236ade9894e0d32e52b"
+PKG_VERSION="3365b1774bc8680be9899968fe45b224ad2f11c1"
 PKG_LICENSE="MAME"
 PKG_SITE="https://github.com/libretro/picodrive"
 PKG_URL="${PKG_SITE}.git"
