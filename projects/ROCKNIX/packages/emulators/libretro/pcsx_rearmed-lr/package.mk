56b796a3ded667895e02653f6e49e47881bbf50a
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-12-15 22:00:42 -0600

    Update pcsx_rearmed-lr package version

--- projects/ROCKNIX/packages/emulators/libretro/pcsx_rearmed-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/pcsx_rearmed-lr/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="pcsx_rearmed-lr"
-PKG_VERSION="9059485691c44cb3a555464b06eddfb1082d586c"
+PKG_VERSION="8d91fbdff8f4db6e256370f030a9f6ac8c7f2c62"
 PKG_ARCH="arm aarch64"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://github.com/libretro/pcsx_rearmed"
