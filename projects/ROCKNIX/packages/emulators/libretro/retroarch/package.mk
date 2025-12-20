6c8e7a7f1e6f1be59bf60c273264dce52b600218
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2025-12-20 05:37:10 -0600

    Update retroarch package version

--- projects/ROCKNIX/packages/emulators/libretro/retroarch/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/retroarch/package.mk
@@ -3,7 +3,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="retroarch"
-PKG_VERSION="831add1e12f7ac4fe29d33ce732587f8a7ffce28" # On going updates. For testing.
+PKG_VERSION="9be7ec925d0db5a21d3b864fa9ca76e3c1fa3cdb" # On going updates. For testing.
 PKG_SITE="https://github.com/libretro/RetroArch"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
 PKG_LICENSE="GPLv3"
