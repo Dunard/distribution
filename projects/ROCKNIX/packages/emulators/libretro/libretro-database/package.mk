a4c87d2e60e111b9c4c982ed8676c2e317b43b3d
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2026-01-01 11:07:29 -0600

    Update libretro-database package version

--- projects/ROCKNIX/packages/emulators/libretro/libretro-database/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/libretro-database/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="libretro-database"
-PKG_VERSION="3b049cde2a6d475535ee09ea49668107caeba115"
+PKG_VERSION="d123da237a654f7cb6c8986d387bc0358053b2dd"
 PKG_LICENSE="GPL"
 PKG_SITE="https://github.com/libretro/libretro-database"
 PKG_URL="https://github.com/libretro/libretro-database/archive/${PKG_VERSION}.tar.gz"
