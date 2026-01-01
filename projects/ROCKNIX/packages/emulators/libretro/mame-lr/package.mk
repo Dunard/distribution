daf2db14c2f2f689d66c4a51e906d5fa7080110b
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2026-01-01 10:59:27 -0600

    Update mame-lr package version and repository links
    libretro version.

--- projects/ROCKNIX/packages/emulators/libretro/mame-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/mame-lr/package.mk
@@ -2,10 +2,10 @@
 # Copyright (C) 2019 Trond Haugland (trondah@gmail.com)
 
 PKG_NAME="mame-lr"
-PKG_VERSION="bf149d327ba85ad6aecac992865630947db5d47d"
+PKG_VERSION="b207f9c6fc93b9d21417a9e4424607840c71db6b"
 PKG_LICENSE="GPLv2"
-PKG_SITE="https://github.com/Hancock33/lr-mame"
-PKG_URL="https://github.com/Hancock33/lr-mame/archive/${PKG_VERSION}.tar.gz"
+PKG_SITE="https://github.com/libretro/mame"
+PKG_URL="https://github.com/libretro/mame/archive/${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain zlib flac sqlite expat"
 PKG_LONGDESC="MAME - Multiple Arcade Machine Emulator"
 PKG_TOOLCHAIN="make"
