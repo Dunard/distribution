2256dc34fe1a9b7ceea944cea5d7dcf97e646bc5
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2026-01-16 11:23:58 -0600

    Update dice-lr package version

--- projects/ROCKNIX/packages/emulators/libretro/dice-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/dice-lr/package.mk
@@ -2,7 +2,7 @@
 # Copyright (C) 2024-present ROCKNIX (https://github.com/ROCKNIX)
 
 PKG_NAME="dice-lr"
-PKG_VERSION="d095d5de9399f0612db35c73a639406ec3e74ca6"
+PKG_VERSION="f41ed433ed90716521b05437c49684c370faa9df"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/mittonk/dice-libretro"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
