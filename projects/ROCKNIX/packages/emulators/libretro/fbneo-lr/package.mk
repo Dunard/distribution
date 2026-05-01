08eac6934bb68858931cea9606bebce76fcf3b1e
Mitchell Hancock <31096423+Dunard@users.noreply.github.com>
2026-05-01 03:04:42 -0500

    Update fbneo-lr package version

--- projects/ROCKNIX/packages/emulators/libretro/fbneo-lr/package.mk
+++ projects/ROCKNIX/packages/emulators/libretro/fbneo-lr/package.mk
@@ -4,7 +4,7 @@
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
 
 PKG_NAME="fbneo-lr"
-PKG_VERSION="012dbe9dc11fd5fd765ef92e0ad4c08583862f69"
+PKG_VERSION="806cbc866c973caf442b4b6c6058f867b36bd1fb"
 PKG_LICENSE="Non-commercial"
 PKG_SITE="https://github.com/libretro/FBNeo"
 PKG_URL="${PKG_SITE}/archive/${PKG_VERSION}.tar.gz"
